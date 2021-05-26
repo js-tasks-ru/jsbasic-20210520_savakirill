c291bd23a7b33d7e690051e013dc5dffbfc5a4f7
Kirill Savchenko <savchenko.kyrylo@gmail.com>
2021-05-26 14:08:31 +0300

    Решил все задачи из 1 модуля + вводный

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  const a = str.toLowerCase();
+  let res = (a.includes('1xbet') || a.includes('xxx')) ? true : false;
+  return res;
+  // if (a.includes('1xbet') || a.includes('xxx')) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
