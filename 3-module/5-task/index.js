828f47effeaac6dce308b2c204a47bab4115ddd9
Kirill Savchenko <savchenko.kyrylo@gmail.com>
2021-05-30 23:38:01 +0300

    Решил 5 задачу из 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,28 @@
 function getMinMax(str) {
-  // ваш код...
+  let k = str.replace(/[^0-9.-]/g, ' ');
+  k = k.replace(/\s+/g, ' ');
+  let stringToArr = k.split(' ').map(Number);
+  let result = {
+    min: 0,
+    max: 0,
+  };
+  for (let i = 0; i < stringToArr.length; i++) {
+    if (stringToArr[i] > 0) {
+      if (result.max === 0) {
+        result.max = stringToArr[i];
+      }
+      if (result.max < stringToArr[i]) {
+        result.max = stringToArr[i];
+      }
+    }
+    if (stringToArr[i] <= 0) {
+      if (result.min === 0) {
+        result.min = stringToArr[i];
+      }
+      if (result.min > stringToArr[i]) {
+        result.min = stringToArr[i];
+      }
+    }
+  }
+  return result;
 }
