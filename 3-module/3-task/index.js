b0bf15e7b2cb5939bf956ddc3166a0c37bf1d373
Kirill Savchenko <savchenko.kyrylo@gmail.com>
2021-05-30 20:57:48 +0300

    Решил 2 и 3 задачу из 3 модуля

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,45 @@
 function camelize(str) {
-  // ваш код...
-}
+  let arr = str.split('-');
+  let res = [];
+  res.push(arr[0]);
+  for (var i = 1; i < arr.length; i++) {
+    // console.log(i);
+    // console.log(arr[i]);
+    if (arr[i].length > 0) {
+      function ucFirst(a) {
+        let res = a[0].toUpperCase();
+        for (let i = 1; i < a.length; i++) {
+          res = res + a[i];
+        }
+        // console.log(res);
+        return res;
+      }
+      res.push(ucFirst(arr[i]));
+    }
+  }
+  let ar = res.join('');
+  // console.log(ar);
+  return ar;
+};
+/*
+function camelize(str) {
+  let arr = str.split('-');
+  let res = arr[0];
+  console.log(res)
+  arr.forEach(function (item) {
+    function ucFirst(a) {
+      let res = a[0].toUpperCase();
+      for (let i = 1; i < a.length; i++) {
+        res = res + a[i];
+      }
+      return res;
+    };
+    if (item.length > 1) {
+      res.push(ucFirst(item));
+    };
+  });
+  let ar = res.join('');
+  console.log(ar);
+  return ar;
+};
+ */
