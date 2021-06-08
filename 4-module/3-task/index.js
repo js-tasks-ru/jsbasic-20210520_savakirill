54309f6378d32e736c10f26064229b7565362248
Kirill Savchenko <savchenko.kyrylo@gmail.com>
2021-06-09 00:47:56 +0300

    Решил 3 задачи из модуля 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+    for (let i = 1; i < table.rows.length; i++) {
+        for (let j = 0; j < [...table.rows[i].cells].length; j++) {
+            if (table.rows[i].cells[j].getAttribute('data-available') === 'true') {
+                table.rows[i].classList.add('available');
+            } else if (table.rows[i].cells[j].getAttribute('data-available') === 'false') {
+                table.rows[i].classList.add('unavailable');
+            }
+        }
+    }
+    for (let i = 1; i < table.rows.length; i++) {
+        if ((table.rows[i].classList.contains('available') === false) && (table.rows[i].classList.contains('unavailable') === false)) {
+            table.rows[i].hidden = 'true';
+        }
+        if (table.rows[i].cells[2].innerHTML === 'm') {
+            table.rows[i].classList.add('male');
+        } else if (table.rows[i].cells[2].innerHTML === 'f') {
+            table.rows[i].classList.add('female');
+        }
+        if (table.rows[i].cells[1].innerHTML < 18) {
+            table.rows[i].style.textDecoration = 'line-through';
+        }
+    }
 }
