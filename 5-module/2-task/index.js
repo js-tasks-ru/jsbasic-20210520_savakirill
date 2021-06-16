5a608a0653538227ca0cad075517b1c33fe81625
Kirill Savchenko <savchenko.kirill1992@gmail.com>
2021-06-16 12:42:46 +0300

    Задачи из 5 блока

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
-  // ваш код...
+    let button = document.querySelector('.toggle-text-button');
+    let text = document.querySelector('#text');
+    let toDo = () => {text.hidden === false ? text.hidden = true: text.hidden = false;}
+    button.addEventListener('click', toDo);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  let currentSliderPosition = 0;
+  let moveTo = 0;
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  buttonLeft.style.display = 'none';
+  let movePoint = [...document.querySelector('.carousel__inner').children][0].offsetWidth;
+  let elemToMove = document.querySelector('.carousel__inner');
+
+  function displayArrow () {
+    if (currentSliderPosition === 0) {
+      buttonLeft.style.display = 'none';
+    } else if (currentSliderPosition === -2964) {
+      buttonRight.style.display = 'none';
+    } else {
+      buttonLeft.style.display = '';
+      buttonRight.style.display = '';
+    }
+  }
+
+  let slideToLeft = () => {
+    moveTo = currentSliderPosition + movePoint;
+    elemToMove.style.transform = `translateX(${moveTo}px)`;
+    currentSliderPosition = currentSliderPosition + movePoint;
+    displayArrow();
+  };
+  let slideToRight = () => {
+    moveTo = currentSliderPosition - movePoint;
+    elemToMove.style.transform = `translateX(${moveTo}px)`;
+    currentSliderPosition = currentSliderPosition - movePoint;
+    displayArrow();
+  };
+  buttonLeft.addEventListener('click', slideToLeft);
+  buttonRight.addEventListener('click', slideToRight);
 }
