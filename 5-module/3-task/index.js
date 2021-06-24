a182cff789032ca720567631fa633163547c3f94
Kirill Savchenko <savchenko.kyrylo@gmail.com>
2021-06-24 11:21:29 +0300

    Решил задачи из 5 модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,70 @@
+/*
 function initCarousel() {
-  // ваш код...
+  let currentSliderPosition = 0;
+  let moveTo = 0;
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  buttonLeft.style.display = 'none';
+  let movePoint = [...document.querySelector('.carousel__inner').children][0].offsetWidth;
+  let elemToMove = document.querySelector('.carousel__inner');
+
+  function displayArrow () {
+    if (currentSliderPosition === 0) {
+      buttonLeft.style.display = 'none';
+      buttonRight.style.display = '';
+    } else if (currentSliderPosition === -2964) {
+      buttonLeft.style.display = '';
+      buttonRight.style.display = 'none';
+    } else {
+      buttonLeft.style.display = '';
+      buttonRight.style.display = '';
+    }
+  }
+
+  let slideToLeft = () => {
+    moveTo = currentSliderPosition + movePoint;
+    elemToMove.style.transform = `translateX(${moveTo}px)`;
+    currentSliderPosition = currentSliderPosition + movePoint;
+    displayArrow();
+  };
+  let slideToRight = () => {
+    moveTo = currentSliderPosition - movePoint;
+    elemToMove.style.transform = `translateX(${moveTo}px)`;
+    currentSliderPosition = currentSliderPosition - movePoint;
+    displayArrow();
+  };
+  buttonLeft.addEventListener('click', slideToLeft);
+  buttonRight.addEventListener('click', slideToRight);
+}
+*/
+function initCarousel() {
+  let currentSliderPosition = 0;
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  buttonLeft.style.display = 'none';
+  let elemToMove = document.querySelector('.carousel__inner');
+  function displayArrow() {
+    if (currentSliderPosition === 0) {
+      buttonLeft.style.display = 'none';
+    } else if (currentSliderPosition === 3) {
+      buttonRight.style.display = 'none';
+    } else {
+      buttonLeft.style.display = '';
+      buttonRight.style.display = '';
+    }
+  }
+  let slideToLeft = () => {
+    currentSliderPosition--;
+    const offset = -elemToMove.offsetWidth * currentSliderPosition;
+    elemToMove.style.transform = `translateX(${offset}px)`;
+    displayArrow();
+  };
+  let slideToRight = () => {
+    currentSliderPosition++;
+    const offset = -elemToMove.offsetWidth * currentSliderPosition;
+    elemToMove.style.transform = `translateX(${offset}px)`;
+    displayArrow();
+  };
+  buttonLeft.addEventListener('click', slideToLeft);
+  buttonRight.addEventListener('click', slideToRight);
 }
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,31 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  };
+  render() {
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+    table.append(tbody);
+    this.rows.forEach((element) => {
+      let tr = document.createElement('tr');
+      for (let value of Object.values(element)) {
+        let td = document.createElement('td');
+        td.innerHTML = value;
+        tr.append(td);
+      }
+      let td = document.createElement('td');
+      let button = document.createElement('button');
+      button.innerHTML = 'X';
+      button.addEventListener('click', this.delRow)
+      td.append(button);
+      tr.append(td);
+      tbody.append(tr);
+    });
+    return table;
+  }
+  delRow(event) {
+    event.target.closest('tr').remove();
   }
 }
