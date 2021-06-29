976b179985a798def5215b0210c2741d01e4f061
Kirill Savchenko <savchenko.kirill1992@gmail.com>
2021-06-29 14:09:13 +0300

    6-3 - нужна редакция

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,33 +1,29 @@
 function initCarousel() {
   let currentSliderPosition = 0;
-  let moveTo = 0;
   let buttonRight = document.querySelector('.carousel__arrow_right');
   let buttonLeft = document.querySelector('.carousel__arrow_left');
   buttonLeft.style.display = 'none';
-  let movePoint = [...document.querySelector('.carousel__inner').children][0].offsetWidth;
   let elemToMove = document.querySelector('.carousel__inner');
-
-  function displayArrow () {
+  function displayArrow() {
     if (currentSliderPosition === 0) {
       buttonLeft.style.display = 'none';
-    } else if (currentSliderPosition === -2964) {
+    } else if (currentSliderPosition === 3) {
       buttonRight.style.display = 'none';
     } else {
       buttonLeft.style.display = '';
       buttonRight.style.display = '';
     }
   }
-
   let slideToLeft = () => {
-    moveTo = currentSliderPosition + movePoint;
-    elemToMove.style.transform = `translateX(${moveTo}px)`;
-    currentSliderPosition = currentSliderPosition + movePoint;
+    currentSliderPosition--;
+    const offset = -elemToMove.offsetWidth * currentSliderPosition;
+    elemToMove.style.transform = `translateX(${offset}px)`;
     displayArrow();
   };
   let slideToRight = () => {
-    moveTo = currentSliderPosition - movePoint;
-    elemToMove.style.transform = `translateX(${moveTo}px)`;
-    currentSliderPosition = currentSliderPosition - movePoint;
+    currentSliderPosition++;
+    const offset = -elemToMove.offsetWidth * currentSliderPosition;
+    elemToMove.style.transform = `translateX(${offset}px)`;
     displayArrow();
   };
   buttonLeft.addEventListener('click', slideToLeft);
